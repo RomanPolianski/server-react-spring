54c622dd945affb8d79e0fca332aa6589bfe7cff
Roman <rpol123453@gmail.com>
2022-03-24 18:26:31 +0300

    Add login request

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import projectsInfo from './projects.js';
 
 const PORT = process.env.PORT ?? 3001;
 const app = express();
+app.use(express.json());
 
 app.get('/projects', (req, res) => {
   try {
@@ -11,6 +12,23 @@ app.get('/projects', (req, res) => {
     res.status(500).send();
   }
 });
+
+app.post('/login', (req, res) => {
+    const user = {
+      username: 'admin',
+      password: '1234'
+    };
+
+    try {
+      if (user.username === req.body.username && user.password === req.body.password) {
+        res.status(200).send('Success');
+      } else {
+        res.status(401).send('Invalid username or password');
+      }
+    } catch {
+      res.status(500).send();
+    }
+  });
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}...`);
