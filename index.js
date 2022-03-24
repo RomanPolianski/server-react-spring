845770010e6a41f61d3a45bd0f6e9f20ce98cae3
Roman <rpol123453@gmail.com>
2022-03-24 17:04:12 +0300

    Fix eslint

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import projectsInfo from './projects.js';
 
 const PORT = process.env.PORT ?? 3001;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello!!!</h1>')
-})
+app.get('/projects', (req, res) => {
+  try {
+    res.send(projectsInfo);
+  } catch {
+    res.status(500).send();
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server has been started on port ${PORT}...`)
+  console.log(`Server has been started on port ${PORT}...`);
 });
