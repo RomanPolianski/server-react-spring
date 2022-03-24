e59da17dc5592723df421f96b1cf6e84f5c6206b
Roman <rpol123453@gmail.com>
2022-03-24 23:39:32 +0300

    Fix cors issue

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import projectsInfo from './projects.js';
+const cors = require('cors');
 
 const PORT = process.env.PORT ?? 3001;
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 
 app.get('/projects', (req, res) => {
   try {
@@ -14,21 +16,24 @@ app.get('/projects', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-    const user = {
-      username: 'admin',
-      password: '1234'
-    };
+  const user = {
+    username: 'admin',
+    password: '1234',
+  };
 
-    try {
-      if (user.username === req.body.username && user.password === req.body.password) {
-        res.status(200).send('Success');
-      } else {
-        res.status(401).send('Invalid username or password');
-      }
-    } catch {
-      res.status(500).send();
+  try {
+    if (
+      user.username === req.body.username &&
+      user.password === req.body.password
+    ) {
+      res.status(200).send('Success');
+    } else {
+      res.status(401).send('Invalid username or password');
     }
-  });
+  } catch {
+    res.status(500).send();
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}...`);
