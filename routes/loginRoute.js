cfe9a1d779a5fff745e773b54d38b37654a62ac9
Roman <rpol123453@gmail.com>
2022-03-28 16:31:56 +0300

    Fix Routes

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router = express.Router();
+import { Router } from 'express';
+const router = Router();
 
 router.post((req, res) => {
   const user = {
@@ -21,4 +21,4 @@ router.post((req, res) => {
   }
 });
 
-module.exports = router;
+export default router;
