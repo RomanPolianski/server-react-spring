d313bfdc1b32e4d30422a91f70510f989ad3c42b
Roman <rpol123453@gmail.com>
2022-04-06 16:42:16 +0300

    Change token lifespan

--- jwt-helper/jwt-helper.js
+++ jwt-helper/jwt-helper.js
@@ -3,10 +3,10 @@ const jwt = require('jsonwebtoken');
 function generateTokens({ id, username }) {
   const user = { id, username };
   const accessToken = jwt.sign(user, process.env.JWT_ACCESS_SECRET, {
-    expiresIn: '30s',
+    expiresIn: '1h',
   });
   const refreshToken = jwt.sign(user, process.env.JWT_REFRESH_SECRET, {
-    expiresIn: '1m',
+    expiresIn: '2h',
   });
   return { accessToken, refreshToken };
 }
