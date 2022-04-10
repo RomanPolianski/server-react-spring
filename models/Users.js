63730dcc7e24ddb9ba9c0c3bf7bc75c270432675
Roman <rpol123453@gmail.com>
2022-04-10 19:29:32 +0300

    Fix relation issue

--- models/Users.js
+++ models/Users.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 module.exports = function (sequelize) {
   return sequelize.define(
-    'Users',
+    'users',
     {
       username: {
         type: Sequelize.STRING,
